ab742f67e7858aeaea21d77aea82e484871abec0
Администратор <k9313219293@gmail.com>
2019-04-24 19:09:44 +0300

    add hello

--- src/shop/Main.java
+++ src/shop/Main.java
@@ -15,7 +15,7 @@ public class Main {
                 300000,
                 2);
         System.out.println(shop.toString());
-
+        System.out.println("hello");
         ServiceShop serviceShop=new ServiceShopImpl(shop);
 
         serviceShop.addNewItem(new Item(500,"ikra krasnaya",20));
